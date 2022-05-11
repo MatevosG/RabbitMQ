7c831dcbce03cb0d1f3e561173111047fa87e8fa
matevos.grigoryan98@gmail.com <matevos.grigoryan98@gmail.com>
2022-05-12 00:32:38 +0400

    Add project files.

---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory 
+            {
+                Uri = new Uri("amqp://guest@localhost:5672")
+            };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare(
+                "demo-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+                );
+            var massage = new {Name="test",Surname="testtest"};
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(massage));
+
+            channel.BasicPublish("","demo-queue",null,body);
+            Console.ReadLine();
+        }
+    }
+}
